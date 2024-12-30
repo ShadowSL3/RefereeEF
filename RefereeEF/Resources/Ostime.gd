910fe89cefc3df9a323e64654c6cd3522b068e67
ShadowSL3 <mizasp71@gmail.com>
2024-12-30 11:50:19 +0100

    Revert "4.4 test"

--- RefereeEF/Resources/Ostime.gd
+++
@@ -1,8 +0,0 @@
-extends Resource
-
-class_name Ostime
-
-
-func time():
-	var current_time = Time.get_datetime_dict_from_system()
-	print("%02d:%02d:%02d" % [current_time.hour, current_time.minute, current_time.second])
